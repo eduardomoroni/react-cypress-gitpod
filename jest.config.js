0736584bc25751fc945b432805f4126cae69cd53
Eduardo Moroni <eduardomoroni@gmail.com>
2021-09-30 07:20:00 +0200

    jest is broken

--- jest.config.js
+++ jest.config.js
@@ -173,10 +173,10 @@ module.exports = {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  transform: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-        '<rootDir>/fileTransformer.js',
-  },
+  // transform: {
+  //   '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+  //       '<rootDir>/fileTransformer.js',
+  // },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
